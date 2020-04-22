c2b5f92a93a6033168bf21252416532d740f10c5
Fabien <fabienamelinck@hotmail.fr>
2020-04-22 10:26:15 +0200

    Modifs ergonomiques

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,15 @@ import("./Echec.js");
 import("./getParam.js");
 
 (function() {
-    var pseudo = $_GET('pseudo'), couleur = $_GET('couleur') // récupère le pseudo et la couleur du joueur
+    let pseudo = $_GET('pseudo'), couleur = $_GET('couleur') // récupère le pseudo et la couleur du joueur
     
-    var view = 0, game, tab;
+    let view , game, tab, poke = document.createElement("audio");
+    poke.setAttribute("autoplay", "");
+    poke.setAttribute("src", "../audios/poke.mp3");;
 
-    const socket = io.connect('http://localhost:8100');
+    const socket = io.connect('http://locahost:8100');
+
+    /**************************    Début partie chat    **************************/
 
     socket.emit('nouveau_client', pseudo);
     document.title = pseudo + ' - ' + document.title;
@@ -30,11 +34,13 @@ import("./getParam.js");
         $('#zone_chat').prepend('<p><strong>' + pseudo + '</strong> ' + message + '</p>');
     }
 
+    /**************************    Fin partie chat    **************************/
 
 
-
+    // demande de récupération de la couleur en fonction du pseudo
     socket.emit('couleur?', pseudo);
 
+    // stockage de la couleur jouée sous forme binaire
     socket.on('couleur', (couleurBin) => {
         document.getElementById("couleur").textContent = "Tu joues les " + ((couleurBin == 0) ? "blancs" : "noirs");
         console.log(pseudo + " " + ((couleurBin == 0) ? "blancs" : "noirs"));
@@ -45,32 +51,41 @@ import("./getParam.js");
     socket.emit('ready?');
 
     // initialisation du plateau
-    socket.on('init', (turn, joueur1, joueur2) => {
+    socket.on('init', (joueur1, joueur2) => {
         console.log(joueur1.pseudo + " contre " + joueur2.pseudo);
 
         tab = document.getElementById("tab");
         while (tab.firstChild) {
             tab.removeChild(tab.lastChild);
         }
+        tab = document.getElementById("poke");
 
         game = new Echec(joueur1, joueur2);
         view = new EchecView(game, "Plateau de jeu", couleur);
 
-        if (turn % 2 == couleur) socket.emit('update', game.grid, game.tour);
+        if (game.getCurrentPlayer() == couleur) socket.emit('update', game.grid, game.tour, game.echec[0]);
     });        
-        
-    socket.on('refresh', (grid, turn) => {
+    
+    // session de jeu
+    socket.on('refresh', (grid, turn, echec) => {
         if (turn > game.tour) { // mise à jour du plateau
             game.tour = turn;
             game.grid = grid;
+            game.echec[0] = echec;
+            game.fini = undefined;
+            game.isFinished();
+            
+            // joue un son quand je peux de nouveau jouer
+            if (tab.lastElementChild.localName == "audio") tab.removeChild(tab.lastElementChild);
+            tab.appendChild(poke);
 
             view.modif_grid(["refresh"]);
-
             socket.emit('refresh', couleur);
         } else if (turn == game.tour) { // tant que je n'ai pas joué
             socket.emit('refresh', couleur);
         } else {
-            socket.emit('update', game.grid, game.tour); //envoie de la nouvelle grille
+            socket.emit('update', game.grid, game.tour, game.echec[0]); // envoie de la nouvelle grille au serveur
+            view.modif_grid(["refresh"]);
         }
     });
 })();
